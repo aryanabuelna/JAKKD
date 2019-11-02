a4925acb7a9ee92ef9e78523170c18978e595106
EC2 Default User <ec2-user@ip-172-31-41-96.ec2.internal>
2019-11-02 18:10:25 +0000

    added templates

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import webapp2
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+import jinja2
+import os
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+    
+class EnterInfoHandler(webapp2.RequestHandler):
+    def get(self):  # for a get request
+        the_variable_dict = {
+            "greeting": "Welcome!!!", 
+            "adjective": "splendid"
+        }
+        
+        locations_template = the_jinja_env.get_template('templates/locations.html')
+        self.response.write(locations_template.render(the_variable_dict))
+
+
+class ShowMemeHandler(webapp2.RequestHandler):
+    def get(self):  # for a get request
+        mappage_template = the_jinja_env.get_template('templates/mappage.html')
+        the_variable_dict = {
+            "line1": "Hurrah for Utsab!", 
+            "line2": "Hurrah!!!!!!!!", 
+            "img_url": "http://www.atozpictures.com/admin/uploads/2016/11/cute-cockatiel-wallpapers.jpg"
+        }
+        self.response.write(mappage_template.render(the_variable_dict))
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', EnterInfoHandler),
+    ('/showmeme', ShowMemeHandler),
 ], debug=True)
